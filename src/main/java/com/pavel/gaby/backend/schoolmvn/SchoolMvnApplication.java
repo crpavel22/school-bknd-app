215244c76e547b0534205460088aa9e45aee14c9
pavel <crpavel@icloud.com>
2019-07-19 21:49:44 -0400

    Carga inicial.

---
+++ src/main/java/com/pavel/gaby/backend/schoolmvn/SchoolMvnApplication.java
@@ -0,0 +1,13 @@
+package com.pavel.gaby.backend.schoolmvn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolMvnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolMvnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pavel/gaby/backend/schoolmvn/SchoolMvnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pavel.gaby.backend.schoolmvn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolMvnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
